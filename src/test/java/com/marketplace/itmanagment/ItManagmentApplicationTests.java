9e7806495c8a3adc22d0867a0f5a30e60cfdf164
simodev744 <ammar.mohamed744@gmail.com>
2025-06-11 14:05:42 +0200

    first commit

---
+++ src/test/java/com/marketplace/itmanagment/ItManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.itmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
